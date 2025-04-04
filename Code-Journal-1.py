92a916ceee3e87de2fc61b25e81c737feb880927
Jose-Sotelo-Nyx <soteloadrian006@gmail.com>
2025-04-03 18:02:54 -0700

    Code-Journal-1
    gives you full name, pronouns, movie description, and favorite food

---
+++ Code-Journal-1.py
@@ -0,0 +1,3 @@
+print("Name: Jose Adrian Sotelo Nunez\nPronouns: He/Him")
+print("Movie Description: There's a vr game that most of the world plays and that the creator left behind his inheritance \nto the game inside the game for whoever finds it will become the owner of said game.\nAfter this became public the whole player base went on an egghunt to find the inheritance")
+print("Favorite Food: Sushi")
